5c9453f3644bc04e70c56138c612dcc2815afb25
kodiakHA <kodiakHA@users.noreply.github.com>
2026-06-07 22:09:07 +0200

    Ajustar tipos de archivos estáticos

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const mimeTypes = {
   ".html": "text/html; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
   ".png": "image/png",
   ".svg": "image/svg+xml",
   ".webp": "image/webp"
